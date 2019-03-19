7323e283be9a666750555f15c6af0ae773cf7dcb
grm27 <gabrielraul.marini@mail.polimi.it>
2019-03-19 17:21:40 +0100

    Aggiunto commento inutile

--- src/main/java/it/polimi/se2019/App.java
+++ src/main/java/it/polimi/se2019/App.java
@@ -4,6 +4,7 @@ package it.polimi.se2019;
  * Hello world!
  */
 public class App {
+    //commento inutile
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
