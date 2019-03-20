46a6b5d662dda259a9df3215d98cdbf3fb010870
fedy97 <federico.morreale@mail.polimi.it>
2019-03-20 23:11:46 +0100

    Aggiunto commento inutile

--- src/main/java/it/polimi/se2019/App.java
+++ src/main/java/it/polimi/se2019/App.java
@@ -6,6 +6,7 @@ package it.polimi.se2019;
 public class App {
     //commento inutile
     //commento inutile pt.2
+    //commento inutile pt.3 morre
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
