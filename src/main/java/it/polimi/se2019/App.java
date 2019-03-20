9e1cda2f97db132bb13ce4ecdb2a4ea284425bbf
simoneorlando21 <simox.xyz@gmail.com>
2019-03-20 21:55:48 +0100

    Commit inutile di prova

--- src/main/java/it/polimi/se2019/App.java
+++ src/main/java/it/polimi/se2019/App.java
@@ -5,6 +5,7 @@ package it.polimi.se2019;
  */
 public class App {
     //commento inutile
+    //commento inutile pt.2
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
