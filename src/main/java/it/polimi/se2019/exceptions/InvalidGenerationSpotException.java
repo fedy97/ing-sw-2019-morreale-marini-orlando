dc115460f8cda9cfa030afc237eca43cfb2a9ea6
fedy97 <federico.morreale@mail.polimi.it>
2019-04-12 21:08:55 +0200

    added exceptions

---
+++ src/main/java/it/polimi/se2019/exceptions/InvalidGenerationSpotException.java
@@ -0,0 +1,4 @@
+package it.polimi.se2019.exceptions;
+
+public class InvalidGenerationSpotException extends Exception {
+}
--- src/main/java/it/polimi/se2019/model/board/Table.java
+++ src/main/java/it/polimi/se2019/model/board/Table.java
@@ -1,7 +1,10 @@
 package it.polimi.se2019.model.board;
 
+import it.polimi.se2019.exceptions.InvalidDeckException;
+import it.polimi.se2019.exceptions.InvalidGenerationSpotException;
 import it.polimi.se2019.model.card.weapons.WeaponCard;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
@@ -11,13 +14,15 @@ import java.util.Map;
  */
 public class Table {
 
-    private Map<Platform, WeaponCard[]> availableWeapons;
+    private Map<Platform, ArrayList<WeaponCard>> availableWeapons;
     private SkullsBoard skullsBoard;
     private GameField gameField;
     private ScoreBoard scoreBoard;
 
-    public Table(Map<Platform, WeaponCard[]> availableWeapons,
-                 SkullsBoard skullsBoard, GameField gameField, ScoreBoard scoreBoard) {
+    public Table(Map<Platform, ArrayList<WeaponCard>> availableWeapons,
+                 SkullsBoard skullsBoard, GameField gameField, ScoreBoard scoreBoard) throws InvalidDeckException {
+        if (availableWeapons.keySet().size() != 3 || availableWeapons.keySet().contains(null) || !isAvailableWeaponsFull())
+            throw new InvalidDeckException("something went wrong while building available weapons, please check again your arguments");
         this.availableWeapons = availableWeapons;
         this.skullsBoard = skullsBoard;
         this.gameField = gameField;
@@ -28,33 +33,35 @@ public class Table {
     /**
      * @return a map of available weapons(3 max) in each generation spot
      */
-    public Map<Platform, WeaponCard[]> getAllAvailableWeapons() {
+    public Map<Platform, ArrayList<WeaponCard>> getAllAvailableWeapons() {
         return availableWeapons;
     }
 
     /**
      * @param generationSpot in which the weapons can be grabbed
-     * @return an array of weapon cards in a specified generation spot
+     * @return an arraylist of weapon cards in a specified generation spot
      */
-    public WeaponCard[] getPlatformAvailableWeapons(Platform generationSpot) {
+    public ArrayList<WeaponCard> getPlatformAvailableWeapons(Platform generationSpot) throws InvalidGenerationSpotException {
+        if (!generationSpot.isGenerationSpot())
+            throw new InvalidGenerationSpotException();
         return availableWeapons.get(generationSpot);
     }
 
     /**
      * @param generationSpot where the weapons are located
-     * @param weaponsToSet to be added to the current available weapons that are less than 3
-     * @return old available weapons, null if wrong generation spot
+     * @param weaponsToSet   to be added to the current available weapons that are less than 3
      */
-    public WeaponCard[] setPlatformAvailableWeapons(Platform generationSpot, WeaponCard[] weaponsToSet) {
-        WeaponCard[] newWeapons = new WeaponCard[availableWeapons.get(generationSpot).length + weaponsToSet.length];
-        for (int i = 0; i < availableWeapons.get(generationSpot).length; i++) {
-            newWeapons[i] = availableWeapons.get(generationSpot)[i];
+    public void setPlatformAvailableWeapons(Platform generationSpot, ArrayList<WeaponCard> weaponsToSet) throws InvalidGenerationSpotException,
+            InvalidDeckException {
+        if (!generationSpot.isGenerationSpot())
+            throw new InvalidGenerationSpotException();
+        if (weaponsToSet.size() > 3)
+            throw new InvalidDeckException("you cannot set more than 3 weapons");
+        if (weaponsToSet.isEmpty())
+            throw new InvalidDeckException("you cannot set an empty collection of weapon cards");
+        for (int i = 0; i < 3 - availableWeapons.get(generationSpot).size(); i++) {
+            availableWeapons.get(generationSpot).add(weaponsToSet.get(i));
         }
-        int j = 0;
-        for (int i = availableWeapons.get(generationSpot).length; i < newWeapons.length; i++) {
-            newWeapons[i] = weaponsToSet[j++];
-        }
-        return availableWeapons.replace(generationSpot, newWeapons);
     }
 
     public SkullsBoard getSkullsBoard() {
@@ -67,6 +74,17 @@ public class Table {
 
     public ScoreBoard getScoreBoard() {
         return scoreBoard;
+    }
+
+    /**
+     * @return true if every platform that holds weapons has 3! weapons ready to be grabbed
+     */
+    private boolean isAvailableWeaponsFull() {
+        boolean isFull = true;
+        for (ArrayList<WeaponCard> arrWep : availableWeapons.values()) {
+            if (arrWep.size() != 3) isFull = false;
+        }
+        return isFull;
     }
 
 }
