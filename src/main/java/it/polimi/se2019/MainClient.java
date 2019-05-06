ddf7fde227b621d304b14a53af8aac378aa758b0
simoneorlando21 <simox.xyz@gmail.com>
2019-05-06 23:53:34 +0200

    Added the Track class to menage the execution of audio files and added the soundtrack to the initial menu.

--- src/main/java/it/polimi/se2019/MainClient.java
+++ src/main/java/it/polimi/se2019/MainClient.java
@@ -2,17 +2,27 @@ package it.polimi.se2019;
 
 
 import it.polimi.se2019.utils.HandyFunctions;
+import it.polimi.se2019.utils.Track;
 import it.polimi.se2019.view.client.RemoteView;
 import it.polimi.se2019.view.client.cli.CLI;
 import it.polimi.se2019.view.client.cli.CliPrinter;
 import it.polimi.se2019.view.client.cli.CliSetUp;
 import it.polimi.se2019.view.client.gui.GUI;
 
+
+import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class MainClient {
 
     public static void main(String[] args) {
+
+        Path path = Paths.get(URI.create(ClassLoader.getSystemResource("soundtrack.wav").toString()));
+
+        Thread track = new Thread(new Track(path.toString()));
+        track.start();
 
         RemoteView myView;
         int choice;
---
+++ src/main/java/it/polimi/se2019/utils/Track.java
@@ -0,0 +1,45 @@
+package it.polimi.se2019.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+/**
+ * @author Simone Orlando
+ */
+public class Track implements Runnable{
+
+
+    private String path;
+    private Clip clip;
+    private AudioInputStream audioInputStream;
+
+    public Track(String path) {
+        this.path = path;
+        try {
+            audioInputStream = AudioSystem.getAudioInputStream(new File(path).getAbsoluteFile());
+        }
+        catch (IOException|UnsupportedAudioFileException e) {
+
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            clip.start();
+        }
+        catch (LineUnavailableException|IOException e) {
+
+        }
+    }
+}
