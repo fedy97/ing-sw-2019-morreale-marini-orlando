927a1f01756ef7091f4cbb59d8016499365b8ccf
fedy97 <federico.morreale@mail.polimi.it>
2019-05-07 19:13:50 +0200

    bug fixed  on Lobby class (null pointer ex)

--- src/main/java/it/polimi/se2019/Lobby.java
+++ src/main/java/it/polimi/se2019/Lobby.java
@@ -15,34 +15,13 @@ import java.util.logging.Level;
 public class Lobby {
 
     private static final String LOCALHOST = "127.0.0.1";
-    private static List<String> users;
+    private static List<String> users = new ArrayList<>();
 
     public static void main(String[] args) {
-        List<String> weapons = new ArrayList<>();
-        weapons.add("Weapon 1");
-        weapons.add("Weapon 2");
-        users = new ArrayList<>();
         RMIServer rmiServer = new RMIServer(1099);
         SocketServer socketServer = new SocketServer(1100);
         rmiServer.start();
         socketServer.start();
-        /*
-        Client client3 = new SocketClient(null, "User3");
-        Client client1 = new RMIClient(null, 1234, "User1");
-        Client client2 = new RMIClient(null, 1345, "User2");
-        Client client4 = new SocketClient(null, "User4");
-
-        try {
-            client3.connect(LOCALHOST, 1100);
-            client1.connect(LOCALHOST, 1099);
-            client2.connect(LOCALHOST, 1099);
-            client4.connect(LOCALHOST, 1100);
-
-            rmiServer.sendToClient(new ShowWeaponsMessage(weapons), "User1");
-            socketServer.sendToClient(new ShowWeaponsMessage(weapons), "User4");
-        } catch (Exception e) {
-            HandyFunctions.LOGGER.log(Level.WARNING, e.toString());
-        }*/
 
 
     }
