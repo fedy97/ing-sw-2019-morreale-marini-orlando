87fd4812310f387740c98aec5a44c1169c74dac5
fedy97 <federico.morreale@mail.polimi.it>
2019-05-10 19:44:47 +0200

    added votations for map + timer in chooseMap

---
+++ src/main/java/it/polimi/se2019/utils/TimerLobby.java
@@ -0,0 +1,26 @@
+package it.polimi.se2019.utils;
+
+import it.polimi.se2019.model.Game;
+
+public class TimerLobby extends Thread {
+    private int seconds;
+    public TimerLobby(int seconds) {
+        this.seconds = seconds;
+    }
+
+    @Override
+    public void run() {
+        try {
+            int slept = 0;
+            while (slept < seconds * 1000) {
+                sleep(1000);
+                slept = slept + 1000;
+                Game.getInstance().setSecondsLeftLobby(seconds - slept / 1000);
+            }
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+    }
+}
+
