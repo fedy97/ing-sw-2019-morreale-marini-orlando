0439c352f7035c19d76e366c4b9dff227b284dd9
grm27 <gabrielraul.marini@mail.polimi.it>
2019-05-16 20:33:14 +0200

    Implemented getWeapon(String light) and getPowerUp(String light) in Deserializer to retrieve the reference of the objects by the hashcode received form the client

--- src/main/java/it/polimi/se2019/utils/Deserializer.java
+++ src/main/java/it/polimi/se2019/utils/Deserializer.java
@@ -2,7 +2,11 @@ package it.polimi.se2019.utils;
 
 import it.polimi.se2019.controller.Controller;
 import it.polimi.se2019.model.board.Platform;
+import it.polimi.se2019.model.card.powerups.PowerUpCard;
 import it.polimi.se2019.model.card.weapons.WeaponCard;
+import it.polimi.se2019.model.player.Player;
+
+import java.util.List;
 
 /**
  * Class providing service methods to match the server objects' reference with the corresponding
@@ -22,8 +26,25 @@ public final class Deserializer {
      *
      */
     public static WeaponCard getWeapon(String light) {
-        //TODO
+        List<WeaponCard> weapons = Controller.getInstance().getPlayerManager().getCurrentPlayer().getWeaponCards();
+        for(WeaponCard weaponCard: weapons){
+            if(Integer.toString(weaponCard.hashCode()).equals(light))
+                return weaponCard;
+        }
         return null;
     }
+
+    /**
+     *
+     */
+    public static PowerUpCard getPowerUp(String light, String owner) {
+        Player player = Controller.getInstance().getGame().getPlayer(owner);
+        for(PowerUpCard powerUpCard: player.getPowerUpCards()){
+            if(Integer.toString(powerUpCard.hashCode()).equals(light))
+                return powerUpCard;
+        }
+        return null;
+    }
+
 
 }
