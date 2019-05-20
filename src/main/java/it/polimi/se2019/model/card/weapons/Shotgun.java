2dea73543a8c44eafffd265d04e6e0879b6def09
grm27 <gabrielraul.marini@mail.polimi.it>
2019-05-20 12:05:00 +0200

    Started implementing Shotgun, something to be discussed

--- src/main/java/it/polimi/se2019/model/card/weapons/Shotgun.java
+++ src/main/java/it/polimi/se2019/model/card/weapons/Shotgun.java
@@ -1,10 +1,48 @@
 package it.polimi.se2019.model.card.weapons;
 
 
-public final class Shotgun extends WeaponAlternativeFire {
+import it.polimi.se2019.controller.Controller;
+import it.polimi.se2019.controller.PlayerManager;
+import it.polimi.se2019.model.Game;
+import it.polimi.se2019.model.board.Platform;
+import it.polimi.se2019.model.enumeration.AmmoCube;
+import it.polimi.se2019.model.enumeration.Character;
+import it.polimi.se2019.model.player.Player;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class Shotgun extends WeaponCard {
 
 
-    public Shotgun() {
+    public Shotgun(AmmoCube paidCost, AmmoCube[] extraCost) {
+        super(paidCost, extraCost);
+        Effect eff1 = new Effect(new AmmoCube[]{});
+        Effect eff2 = new Effect(new AmmoCube[]{AmmoCube.RED, AmmoCube.BLUE});
+        PlayerManager playerManager = Controller.getInstance().getPlayerManager();
+        Game game = Controller.getInstance().getGame();
+        List<Character> targets = playerManager.getCurrentPlayer().getCurrentPlatform().getPlayersOnThePlatform();
+        Map<Player, Integer> damages = new HashMap<>();
+
+        BasicEffect be1 = c -> {
+            Platform currentPlatform = c.getPlayerManager().getCurrentPlayer().getCurrentPlatform();
+            c.askFor(currentPlatform.getPlayersOnThePlatform(), "targets");
+        };
+        eff1.addBasicEffect(be1);
+
+        BasicEffect be2 = c -> {
+            Map<Player, Integer> damageMap = new HashMap<>();
+            damageMap.put(c.getCurrentTargets().get(0), 3);
+            c.getPlayerManager().addDamage(damageMap);
+        };
+        eff1.addBasicEffect(be1);
+
+        //TODO
+        eff2.addBasicEffect(be2);
+
+        addEffect(eff1);
+        addEffect(eff2);
     }
 
 }
