099617a3c37138a9cbe7344a24b45bc0601f7942
fedy97 <federico.morreale@mail.polimi.it>
2019-05-24 13:41:54 +0200

    GUI now displays the right quantity of ammos for each player

--- src/main/java/it/polimi/se2019/view/client/gui/PlayerBoardController.java
+++ src/main/java/it/polimi/se2019/view/client/gui/PlayerBoardController.java
@@ -48,11 +48,23 @@ public class PlayerBoardController {
     private LightGameVersion lightGameVersion;
     private List<ImageView> weaponsImages;
     private List<ImageView> powerupsImages;
+    private List<ImageView> yellowAmmosImages;
+    private List<ImageView> blueAmmosImages;
+    private List<ImageView> redAmmosImages;
     private String currPlayerDisplay;
 
     public void initialize() {
         weaponsImages = new ArrayList<>();
         powerupsImages = new ArrayList<>();
+        yellowAmmosImages = new ArrayList<>();
+        blueAmmosImages = new ArrayList<>();
+        redAmmosImages = new ArrayList<>();
+        yellowAmmosImages.add(yellowAmmo1);
+        yellowAmmosImages.add(yellowAmmo2);
+        blueAmmosImages.add(blueAmmo1);
+        blueAmmosImages.add(blueAmmo2);
+        redAmmosImages.add(redAmmo1);
+        redAmmosImages.add(redAmmo2);
         weaponsImages.add(wImage1);
         weaponsImages.add(wImage2);
         weaponsImages.add(wImage3);
@@ -68,19 +80,38 @@ public class PlayerBoardController {
         for (ImageView weapon : weaponsImages)
             weapon.setImage(new Image("/assets/weapons/back.png"));
         boardImage.setImage(new Image("/assets/boards/" + playerToDisplay.toLowerCase() + ".png"));
+        //update powerups
         Map<String, List<CardRep>> charPowerupsReps = lightGameVersion.getPlayerPowerups();
         List<CardRep> myPowerupsReps = charPowerupsReps.get(playerToDisplay);
         if (myChar.equals(playerToDisplay)) {
             for (CardRep myPowerup : myPowerupsReps)
                 powerupsImages.get(myPowerupsReps.indexOf(myPowerup)).setImage(new Image(myPowerup.getPath()));
         }
-        Map<String,List<CardRep>> charWeapons = lightGameVersion.getPlayerWeapons();
+        //update weapons
+        Map<String, List<CardRep>> charWeapons = lightGameVersion.getPlayerWeapons();
         List<CardRep> myWeaponsReps = charWeapons.get(playerToDisplay);
         for (CardRep myWeapon : myWeaponsReps)
             weaponsImages.get(myWeaponsReps.indexOf(myWeapon)).setImage(new Image(myWeapon.getPath()));
-
-
-
+        //update ammos
+        Map<String, Integer> colorQuantity = lightGameVersion.getPlayerBoardRep().get(playerToDisplay).getColorQtyAmmos();
+        int red = colorQuantity.get("RED");
+        int blue = colorQuantity.get("BLUE");
+        int yellow = colorQuantity.get("YELLOW");
+        for (int i = 0; i < 2; i++) {
+            redAmmosImages.get(i).setVisible(true);
+            if (i >= red)
+                redAmmosImages.get(i).setVisible(false);
+        }
+        for (int i = 0; i < 2; i++) {
+            blueAmmosImages.get(i).setVisible(true);
+            if (i >= blue)
+                blueAmmosImages.get(i).setVisible(false);
+        }
+        for (int i = 0; i < 2; i++) {
+            yellowAmmosImages.get(i).setVisible(true);
+            if (i >= yellow)
+                yellowAmmosImages.get(i).setVisible(false);
+        }
     }
 
     void passGUI(GUI gui) {
