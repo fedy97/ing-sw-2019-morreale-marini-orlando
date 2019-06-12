cc8f22b27db68ac8b4f8129ae79c9d84684e4753
grm27 <gabrielraul.marini@mail.polimi.it>
2019-06-12 20:50:01 +0200

    Implemented and tested Hellion

---
+++ src/main/java/it/polimi/se2019/model/card/weapons/Hellion.java
@@ -0,0 +1,89 @@
+package it.polimi.se2019.model.card.weapons;
+
+
+import it.polimi.se2019.controller.Controller;
+import it.polimi.se2019.controller.PlayerManager;
+import it.polimi.se2019.exceptions.InvalidNameException;
+import it.polimi.se2019.model.Game;
+import it.polimi.se2019.model.enumeration.AmmoCube;
+import it.polimi.se2019.model.enumeration.Character;
+import it.polimi.se2019.model.player.Player;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class Hellion extends WeaponAlternativeFire {
+
+    public Hellion(String name, String descr, String img, AmmoCube paidCost, AmmoCube[] extraCost) throws InvalidNameException {
+        super(name, descr, img, paidCost, extraCost);
+        Controller c = Controller.getInstance();
+        PlayerManager playerManager = c.getPlayerManager();
+        Game game = Controller.getInstance().getGame();
+
+
+        Effect eff1 = new Effect(new AmmoCube[]{}) {
+            @Override
+            public void activateEffect(List<Character> chosenTargets, WeaponCard card) {
+                Player target = game.getPlayer(chosenTargets.get(0));
+
+                Map<Player, Integer> damages = new HashMap<>();
+                damages.put(target, 1);
+                playerManager.addDamage(damages);
+
+                for (Character character : target.getCurrentPlatform().getPlayersOnThePlatform()) {
+                    playerManager.mark(game.getPlayer(character), 1);
+                }
+
+                usableEffects = new boolean[]{false, false, false};
+            }
+
+            @Override
+            public void setupTargets() {
+                List<Character> targets = game.getGameField().getVisiblePlayers(playerManager.getCurrentPlayer().getCurrentPlatform());
+                targets.removeAll(playerManager.getCurrentPlayer().getCurrentPlatform().getPlayersOnThePlatform());
+                this.setPossibleTargets(targets);
+            }
+        };
+
+        Effect eff2 = new Effect(new AmmoCube[]{AmmoCube.RED}) {
+            @Override
+            public void activateEffect(List<Character> chosenTargets, WeaponCard card) {
+                Player target = game.getPlayer(chosenTargets.get(0));
+
+                Map<Player, Integer> damages = new HashMap<>();
+                damages.put(target, 1);
+                playerManager.addDamage(damages);
+
+                for (Character character : target.getCurrentPlatform().getPlayersOnThePlatform()) {
+                    playerManager.mark(game.getPlayer(character), 2);
+                }
+
+                usableEffects = new boolean[]{false, false, false};
+                playerManager.getCurrentPlayer().getPlayerBoard().getAmmoBox().removeAmmos(this.getCost());
+            }
+
+            @Override
+            public void setupTargets() {
+                List<Character> targets = game.getGameField().getVisiblePlayers(playerManager.getCurrentPlayer().getCurrentPlatform());
+                targets.removeAll(playerManager.getCurrentPlayer().getCurrentPlatform().getPlayersOnThePlatform());
+                this.setPossibleTargets(targets);
+            }
+        };
+
+        usableEffects = new boolean[]{true, true, false};
+
+        eff1.setMaxTargets(1);
+        eff2.setMaxTargets(1);
+
+        getEffects().add(eff1);
+        getEffects().add(eff2);
+    }
+
+    @Override
+    public void reload() {
+        cleanCache();
+        usableEffects = new boolean[]{true, true, false};
+        loaded = true;
+    }
+}
