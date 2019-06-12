d8861cd14b2b020d8b91ef27113e3645e783a3a2
grm27 <gabrielraul.marini@mail.polimi.it>
2019-06-12 20:50:26 +0200

    Implemented and tested ZX2

---
+++ src/main/java/it/polimi/se2019/model/card/weapons/ZX2.java
@@ -0,0 +1,74 @@
+package it.polimi.se2019.model.card.weapons;
+
+
+import it.polimi.se2019.controller.Controller;
+import it.polimi.se2019.controller.PlayerManager;
+import it.polimi.se2019.exceptions.InvalidNameException;
+import it.polimi.se2019.model.Game;
+import it.polimi.se2019.model.enumeration.AmmoCube;
+import it.polimi.se2019.model.enumeration.Character;
+import it.polimi.se2019.model.player.Player;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class ZX2 extends WeaponAlternativeFire {
+
+    public ZX2(String name, String descr, String img, AmmoCube paidCost, AmmoCube[] extraCost) throws InvalidNameException {
+        super(name, descr, img, paidCost, extraCost);
+        Controller c = Controller.getInstance();
+        PlayerManager playerManager = c.getPlayerManager();
+        Game game = Controller.getInstance().getGame();
+
+
+        Effect eff1 = new Effect(new AmmoCube[]{}) {
+            @Override
+            public void activateEffect(List<Character> chosenTargets, WeaponCard card) {
+                Player target = game.getPlayer(chosenTargets.get(0));
+
+                Map<Player, Integer> damages = new HashMap<>();
+                damages.put(target, 1);
+                playerManager.addDamage(damages);
+                playerManager.mark(target, 2);
+
+                usableEffects = new boolean[]{false, false, false};
+            }
+
+            @Override
+            public void setupTargets() {
+                this.setPossibleTargets(game.getGameField().getVisiblePlayers(playerManager.getCurrentPlayer().getCurrentPlatform()));
+            }
+        };
+
+        Effect eff2 = new Effect(new AmmoCube[]{}) {
+            @Override
+            public void activateEffect(List<Character> chosenTargets, WeaponCard card) {
+                for (Character character : chosenTargets) {
+                    playerManager.mark(game.getPlayer(character), 1);
+                }
+                usableEffects = new boolean[]{false, false, false};
+            }
+
+            @Override
+            public void setupTargets() {
+                this.setPossibleTargets(game.getGameField().getVisiblePlayers(playerManager.getCurrentPlayer().getCurrentPlatform()));
+            }
+        };
+
+        usableEffects = new boolean[]{true, true, false};
+
+        eff1.setMaxTargets(1);
+        eff2.setMaxTargets(3);
+
+        getEffects().add(eff1);
+        getEffects().add(eff2);
+    }
+
+    @Override
+    public void reload() {
+        cleanCache();
+        usableEffects = new boolean[]{true, true, false};
+        loaded = true;
+    }
+}
