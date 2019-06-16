f822ac7419cf94e9adb69e61a9d6f9fde6080c97
grm27 <gabrielraul.marini@mail.polimi.it>
2019-06-16 12:38:44 +0200

    Implemented Teleporter

--- src/main/java/it/polimi/se2019/model/card/powerups/Teleporter.java
+++ src/main/java/it/polimi/se2019/model/card/powerups/Teleporter.java
@@ -1,11 +1,12 @@
 package it.polimi.se2019.model.card.powerups;
 
 
-import it.polimi.se2019.controller.Controller;
 import it.polimi.se2019.exceptions.InvalidCubeException;
 import it.polimi.se2019.exceptions.InvalidNameException;
 import it.polimi.se2019.model.enumeration.AmmoCube;
+import it.polimi.se2019.model.enumeration.Character;
 import it.polimi.se2019.model.player.Player;
+import it.polimi.se2019.utils.CustomLogger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,9 +16,6 @@ public final class Teleporter extends PowerUpCard {
 
     public Teleporter(AmmoCube ammoCube, String name, String description, String img) throws InvalidNameException, InvalidCubeException {
         super(ammoCube, name, description, img);
-     /*   Action eff1 = c -> c.askFor(c.getGame().getGameField().getPlatforms(), "position");
-        effects.add(eff1);
-        stages.add(0);*/
     }
 
     /**
@@ -30,9 +28,25 @@ public final class Teleporter extends PowerUpCard {
     /**
      * @return a collection of players that can be the target
      */
-    public List<Player> getPossibleTargets(Controller c) {
-        //TODO
-        return new ArrayList<>();
+    @Override
+    public List<Character> getPossibleTargets() {
+        List<Character> targets = new ArrayList<>();
+        for (Player player : c.getGame().getPlayers())
+            targets.add(player.getCharacter());
+        return targets;
+    }
+
+    @Override
+    public void activate(Character target) {
+        c.sendMessage("Where do you want to teleport?", c.getPlayerManager().getCurrentPlayer().getName());
+        c.askFor(game.getGameField().getPlatforms(), "position");
+
+        try {
+            c.getPlayerManager().getCurrentPlayer().setCurrentPlatform(c.getChosenDestination().take());
+            c.broadcastMessage(c.getPlayerManager().getCurrentPlayer().getName() + " teleported!");
+        } catch (Exception e) {
+            CustomLogger.logException(this.getClass().getName(), e);
+        }
     }
 
 }
