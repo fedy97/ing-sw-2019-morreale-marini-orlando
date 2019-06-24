6013fa768e98bebea291d9c8e75be0f1bf09319a
grm27 <gabrielraul.marini@mail.polimi.it>
2019-06-24 10:43:38 +0200

    Tested toString() in AmmoCard

--- src/test/java/it/polimi/se2019/model/card/TestAmmoCard.java
+++ src/test/java/it/polimi/se2019/model/card/TestAmmoCard.java
@@ -1,17 +1,14 @@
 package it.polimi.se2019.model.card;
 
 
-import it.polimi.se2019.exceptions.*;
-import it.polimi.se2019.model.card.AmmoCard;
-import it.polimi.se2019.model.card.Deck;
-import it.polimi.se2019.model.card.powerups.PowerUpCard;
-import it.polimi.se2019.model.enumeration.AmmoCube;
+import it.polimi.se2019.exceptions.InvalidCardException;
+import it.polimi.se2019.exceptions.InvalidDeckException;
+import it.polimi.se2019.exceptions.InvalidImageException;
+import it.polimi.se2019.exceptions.NoCardException;
 import it.polimi.se2019.utils.JsonParser;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -27,15 +24,20 @@ public class TestAmmoCard {
      * AmmoCard
      */
     @Test
-    public void TestAmmoCard() throws InvalidImageException,IOException,InvalidDeckException, NoCardException {
+    public void TestAmmoCard() throws InvalidImageException, IOException, InvalidDeckException, NoCardException {
 
         try {
             JsonParser parser = new JsonParser("/json/ammocards.json");
             Deck<AmmoCard> deck;
             //now ammocards has 36 ammos cards according to json
             deck = parser.buildAmmoCards();
-            //TODO ISSUE
-            //assertTrue(deck.drawCard().hasPowerUp());
-        } catch (InvalidCardException ex) {}
+
+            assertTrue(deck.drawCard().hasPowerUp());
+            AmmoCard card = deck.drawCard();
+            assertEquals("yb", card.toString());
+
+        } catch (InvalidCardException ex) {
+            fail();
+        }
     }
 }
