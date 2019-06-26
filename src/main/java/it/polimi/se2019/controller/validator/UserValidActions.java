abd99c9918f1f0744484d24efe08f5b4daae8f22
grm27 <gabrielraul.marini@mail.polimi.it>
2019-06-26 19:49:56 +0200

    Added new type of actions for frenzy mode

--- src/main/java/it/polimi/se2019/controller/validator/UserValidActions.java
+++ src/main/java/it/polimi/se2019/controller/validator/UserValidActions.java
@@ -4,7 +4,9 @@ public enum UserValidActions {
 
     ALL(new boolean[]{true, true, true, true, true, true, true}),
     NONE(new boolean[]{false, false, false, false, false, false, false}),
-    NO_BASIC(new boolean[]{false, false, false, true, true, true, true});
+    NO_BASIC(new boolean[]{false, false, false, true, true, true, true}),
+    NO_SHOOT(new boolean[]{true, true, false, true, true, true, true}),
+    FRENZY_2(new boolean[]{true, true, false, true, true, true, true});
 
     private boolean[] actions;
 
