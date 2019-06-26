6ca27cb072221b9c0aa7a39f736d8ee44f6f2d92
grm27 <gabrielraul.marini@mail.polimi.it>
2019-06-26 19:49:10 +0200

    Implemented Frenzy validators to support Frenzy actions

--- src/main/java/it/polimi/se2019/controller/validator/Frenzy1Validator.java
+++ src/main/java/it/polimi/se2019/controller/validator/Frenzy1Validator.java
@@ -2,7 +2,11 @@ package it.polimi.se2019.controller.validator;
 
 import it.polimi.se2019.Action;
 import it.polimi.se2019.controller.Controller;
+import it.polimi.se2019.exceptions.InvalidActionException;
+import it.polimi.se2019.model.board.GameField;
 import it.polimi.se2019.model.board.Platform;
+import it.polimi.se2019.model.player.Player;
+import it.polimi.se2019.utils.CustomLogger;
 
 import java.util.*;
 
@@ -16,9 +20,24 @@ public class Frenzy1Validator extends Validator {
     }
 
 
-    public List<Platform> getValidMoves(Action c){
-        //TODO
-        return new ArrayList<>();
+    /**
+     * @param c command received by the player
+     * @return the list of platform destination the player can move to
+     */
+    @Override
+    public List<Platform> getValidMoves(Action c) {
+        List<Platform> res = null;
+        GameField gameField = father.getGame().getGameField();
+        Player currentPlayer = father.getPlayerManager().getCurrentPlayer();
+
+        if (c == Action.SHOOT)
+            res = gameField.getAvailablePlatforms(currentPlayer.getCurrentPlatform(), 1);
+        if (c == Action.MOVE)
+            res = gameField.getAvailablePlatforms(currentPlayer.getCurrentPlatform(), 4);
+        if (c == Action.GRAB)
+            res = gameField.getAvailablePlatforms(currentPlayer.getCurrentPlatform(), 2);
+
+        return res;
     }
 
 }
--- src/main/java/it/polimi/se2019/controller/validator/Frenzy2Validator.java
+++ src/main/java/it/polimi/se2019/controller/validator/Frenzy2Validator.java
@@ -2,7 +2,10 @@ package it.polimi.se2019.controller.validator;
 
 import it.polimi.se2019.Action;
 import it.polimi.se2019.controller.Controller;
+import it.polimi.se2019.exceptions.InvalidActionException;
+import it.polimi.se2019.model.board.GameField;
 import it.polimi.se2019.model.board.Platform;
+import it.polimi.se2019.model.player.Player;
 
 import java.util.*;
 
@@ -15,9 +18,25 @@ public class Frenzy2Validator extends Validator {
         super(father);
     }
 
-    public List<Platform> getValidMoves(Action c) {
-        //TODO
-        return new ArrayList<>();
+    /**
+     * @param c command received by the player
+     * @throws InvalidActionException if move action is passed, cannot move in this mode
+     * @return the list of platform destination the player can move to
+     */
+    @Override
+    public List<Platform> getValidMoves(Action c) throws InvalidActionException{
+        List<Platform> res;
+        GameField gameField = father.getGame().getGameField();
+        Player currentPlayer = father.getPlayerManager().getCurrentPlayer();
+
+        if (c == Action.SHOOT)
+            res = gameField.getAvailablePlatforms(currentPlayer.getCurrentPlatform(), 2);
+        else if (c == Action.GRAB)
+            res = gameField.getAvailablePlatforms(currentPlayer.getCurrentPlatform(), 3);
+        else
+            throw new InvalidActionException("You are in frenzy mode 2, you cannot just move!");
+
+        return res;
     }
 
 }
