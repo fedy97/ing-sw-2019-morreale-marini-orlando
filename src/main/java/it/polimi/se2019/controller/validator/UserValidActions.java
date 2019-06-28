92d99514730d6d1e646c0e68d9986d2ce2e2d24d
grm27 <gabrielraul.marini@mail.polimi.it>
2019-06-28 17:48:24 +0200

    ONLY END ACTION

--- src/main/java/it/polimi/se2019/controller/validator/UserValidActions.java
+++ src/main/java/it/polimi/se2019/controller/validator/UserValidActions.java
@@ -2,11 +2,13 @@ package it.polimi.se2019.controller.validator;
 
 public enum UserValidActions {
 
+    // action 1 - action 2 - action 3 - reload - endturn - powerups - convert
     ALL(new boolean[]{true, true, true, true, true, true, true}),
     NONE(new boolean[]{false, false, false, false, false, false, false}),
     NO_BASIC(new boolean[]{false, false, false, true, true, true, true}),
     NO_SHOOT(new boolean[]{true, true, false, true, true, true, true}),
-    FRENZY_2(new boolean[]{true, true, false, true, true, true, true});
+    FRENZY_2(new boolean[]{true, true, false, true, true, true, true}),
+    ONLY_END(new boolean[]{false, false, false, false, true, true, false});
 
     private boolean[] actions;
 
