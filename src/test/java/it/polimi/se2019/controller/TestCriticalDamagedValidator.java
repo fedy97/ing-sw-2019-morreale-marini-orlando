7bd55ffaf0e1a1c55adac77861b66262e4d0ca17
grm27 <gabrielraul.marini@mail.polimi.it>
2019-06-29 16:13:56 +0200

    Tested DamagedValidator

---
+++ src/test/java/it/polimi/se2019/controller/TestCriticalDamagedValidator.java
@@ -0,0 +1,52 @@
+package it.polimi.se2019.controller;
+
+import it.polimi.se2019.Action;
+import it.polimi.se2019.controller.validator.CriticalDamagedValidator;
+import it.polimi.se2019.controller.validator.Validator;
+import it.polimi.se2019.exceptions.*;
+import it.polimi.se2019.model.player.Player;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * Test class used to verify if the DamagedValidator works properly
+ *
+ * @author Gabriel Raul Marini
+ */
+public class TestCriticalDamagedValidator extends TestControllerChild {
+    private Validator validator;
+    private Player currPlayer;
+
+    @Before
+    public void initTest() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException, InvalidImageException, InvalidNameException, InvalidCubeException, IOException, InvalidDeckException, InvalidCardException {
+        super.initTest();
+        validator = new CriticalDamagedValidator(c);
+        currPlayer = c.getPlayerManager().getCurrentPlayer();
+        currPlayer.setCurrentPlatform(c.getGame().getGameField().getPlatforms().get(0));
+    }
+
+    @Test
+    public void getValidMoves() {
+        try {
+            assertTrue(!validator.getValidMoves(Action.GRAB).isEmpty());
+            assertTrue(!validator.getValidMoves(Action.MOVE).isEmpty());
+            assertTrue(!validator.getValidMoves(Action.SHOOT).isEmpty());
+        } catch (Exception e) {
+            fail();
+
+        }
+    }
+
+    @After
+    public void finisTest() {
+        currPlayer.getWeaponCards().clear();
+        currPlayer.getPowerUpCards().clear();
+    }
+}
