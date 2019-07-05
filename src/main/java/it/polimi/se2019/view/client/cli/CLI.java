b71eeadb480bc538d10587dad609d807c813bbe8
simoneorlando21 <simox.xyz@gmail.com>
2019-07-05 17:47:19 +0200

    Fixed scoreboard in CLI.

--- src/main/java/it/polimi/se2019/view/client/cli/CLI.java
+++ src/main/java/it/polimi/se2019/view/client/cli/CLI.java
@@ -1128,7 +1128,7 @@ public class CLI extends RemoteView {
         CliPrinter.stamp("CLASSIFICA: ", CliColor.TEXTGREEN);
         CliPrinter.stamp("\n");
         for (Map.Entry<String, Integer> s: scoreboard.entrySet()) {
-            CliPrinter.stamp(s + ": " + scoreboard.get(s.getKey()) + "\n");
+            CliPrinter.stamp(s.getValue() + ": " + scoreboard.get(s.getKey()) + "\n");
         }
         System.exit(0);
     }
